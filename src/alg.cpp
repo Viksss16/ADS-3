7322c2550ad3147de0fd27b684f8af86b499f7b3
Viksss16 <160354641+Viksss16@users.noreply.github.com>
2024-03-16 00:04:50 +0300

    Update alg.cpp

--- src/alg.cpp
+++ src/alg.cpp
@@ -1,6 +1,31 @@
 // Copyright 2021 NNTU-CS
 
-int cbinsearch(int *arr, int size, int value) {
-  //  поместить сюда реализацию алгоритма
-  return 0; // если ничего не найдено
+int binarySearchCount(int *arr, int size, int value) {
+    int count = 0;
+    int left = 0;
+    int right = size - 1;
+    while (left <= right) {
+        int mid = left + (right - left) / 2;
+        if (arr[mid] == value) {
+            count++;
+            int temp = mid;
+            while (temp > left && arr[temp - 1] == value) {
+                count++;
+                temp--;
+            }
+            temp = mid;
+            while (temp < right && arr[temp + 1] == value) {
+                count++;
+                temp++;
+            }
+            return count;
+        }
+        else if (arr[mid] < value) {
+            left = mid + 1;
+        }
+        else {
+            right = mid - 1;
+        }
+    }
+    return count;
 }
